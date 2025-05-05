160c005f26523d9c25de2df6aca7e6dd6785a555
JeyasheelaR3104 <sheelamady723@gmail.com>
2025-05-05 19:42:37 +0530

    Updated code

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-if ($(".text-slider").length == 1) {
-    var typed_strings = $(".texts").text();
-    var typed = new Typed(".text-slider", {
-        strings: typed_strings.split(", "),
-        typeSpeed: 50,
-        loop: true,
-        backDelay: 900,
-        backSpeed: 30,
-    });
-}
+// typed js
+var typed = new Typed("#line", {
+    strings: ["Front End Developer!", "Web Developer!", "UI/UX Developer!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1500,
+    loop: true
+  });
